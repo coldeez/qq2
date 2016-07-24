03b5dd4414bc53ea1c9eaa972e6d1a0e64e5ed3b
coldeez <coldywebdis@yandex.ru>
2016-07-24 17:50:22 +0300

    podgotovlen

---
+++ sandbox/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import jssc.SerialPort;
+import jssc.SerialPortException;
+import jssc.SerialPortList;
+
+public class Main {
+
+	public static void main(String[] args) {
+		String[] portNames = SerialPortList.getPortNames();
+		for(int i = 0; i < portNames.length; i++){
+			System.out.println(portNames[i]);
+		}
+	}
+}
--- sandbox/src/main/java/MyFirstProgram.java
+++
@@ -1,9 +0,0 @@
-public class MyFirstProgram {
-
-
-	public static void main(String[] arg) {
-System.out.println("pennis");
-		
-	}
-
-}
