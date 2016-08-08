573b02dc0f16783ba7eeb959a771d207c7837201
coldeez <coldywebdis@yandex.ru>
2016-08-08 23:28:08 +0300

    Добавлен фон без границ

--- sandbox/src/main/java/MainForm.java
+++ sandbox/src/main/java/MainForm.java
@@ -1,22 +1,27 @@
+import javax.imageio.ImageIO;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by kbal on 03.08.2016.
  */
-public class MainForm extends JFrame{
+public class MainForm extends JFrame {
   private JButton resetQuestButton;
   private JButton startQuestButton;
   private JPanel mainPanel;
 
   public MainForm() {
-    setContentPane(mainPanel);
+/*    setContentPane(mainPanel);*/
+    setContentPane(new BgPanel());
     setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-    setSize(1000,1000);
-
-
+    setSize(1000, 1000);
+    /* important Statement */
+    setUndecorated(true);
 
 
     resetQuestButton.addActionListener(new ActionListener() {
@@ -37,5 +42,16 @@ public class MainForm extends JFrame{
 
   }
 
+  class BgPanel extends JPanel {
+    public void paintComponent(Graphics g) {
+      Image im = null;
+      try {
+        im = ImageIO.read(new File("C:\\test\\test.jpg"));
+      } catch (IOException e) {
+      }
+      g.drawImage(im, 0, 0, null);
+    }
 
+
+  }
 }
